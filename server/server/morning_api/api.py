810e401bab1523bd1783bf7a522d02fb17f7afc5
nhrnhr0 <nhrnhr0@gmail.com>
2023-09-06 10:37:46 +0300

    add env variables now

--- server/server/morning_api/api.py
+++ server/server/morning_api/api.py
@@ -125,7 +125,7 @@ class MorningAPI(metaclass=SingletonMeta):
         #}
         url = f'{self.BASE_MORNING_URL}/account/token'
         data = {
-            "id": os.environ["MORNING_API_SECRET"],
+            "id": os.environ["MORNING_API_CLIENT_ID"],
             "secret": os.environ["MORNING_API_SECRET"]
         }
         # response = self.make_request(url, method='POST', data=data, include_auth=False)
