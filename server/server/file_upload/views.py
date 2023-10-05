b136e8842f2206923d660c2a26b645eb755a1b92
kim-gusovsky <kimgusovsky4@gmail.com>
2023-10-05 16:56:44 +0300

    updated the views

--- server/server/file_upload/views.py
+++ server/server/file_upload/views.py
@@ -24,81 +24,154 @@ from django.shortcuts import render
 # from django.shortcuts import get_object_or_404
 # from project.models import Project
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from .models import FileUpload
-from .serializers import MyModelSerializer
+from .serializers import ProjectFilesSerializer
 from django.http import JsonResponse
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.files.base import ContentFile
 from base_project.models import BaseProject
 from rest_framework import status
-@api_view(['GET'])
-def get_files(request,project_id):
-    print(project_id)
-    try:
-        project_files=BaseProject.objects.get(pk=project_id)
-        queryset = project_files.files.all()
-        serializer_class = MyModelSerializer(queryset, many=True)
-        return JsonResponse(serializer_class.data, safe=False)
-    except BaseProject.DoesNotExist:
-        return JsonResponse({'error': 'Project not found'}, status=404)
+# @api_view(['GET'])
+# def get_files(request,project_id):
+#     print(project_id)
+#     try:
+#         project_files=BaseProject.objects.get(pk=project_id)
+#         queryset = project_files.files.all()
+#         serializer_class = ProjectFilesSerializer(queryset, many=True)
+#         return JsonResponse(serializer_class.data, safe=False)
+#     except BaseProject.DoesNotExist:
+#         return JsonResponse({'error': 'Project not found'}, status=404)
 
-@api_view(['POST'])
-def add_new_file(request):
-    try:
-        project_id = request.POST['projectId']
-        project = BaseProject.objects.get(pk=project_id)
+# @api_view(['POST'])
+# def add_new_file(request):
+#     try:
+#         project_id = request.POST['projectId']
+#         project = BaseProject.objects.get(pk=project_id)
         
-        if 'files' in request.FILES:
-            file_list = request.FILES.getlist('files')
-            file_names = request.POST.getlist('fileNames')
+#         if 'files' in request.FILES:
+#             file_list = request.FILES.getlist('files')
+#             file_names = request.POST.getlist('fileNames')
 
-            for i in range(len(file_list)):
-                current_file = FileUpload.objects.create(file_name=file_names[i], file=file_list[i])
-                project.files.add(current_file)
+#             for i in range(len(file_list)):
+#                 current_file = FileUpload.objects.create(file_name=file_names[i], file=file_list[i])
+#                 project.files.add(current_file)
 
-        elif 'file' in request.FILES:
-            file = request.FILES['file']
-            file_name = request.POST['fileName']
-            new_file = FileUpload.objects.create(file_name=file_name, file=file)
-            project.files.add(new_file)
+#         elif 'file' in request.FILES:
+#             file = request.FILES['file']
+#             file_name = request.POST['fileName']
+#             new_file = FileUpload.objects.create(file_name=file_name, file=file)
+#             project.files.add(new_file)
+
+#         else:
+#             return JsonResponse({'error': 'No files were provided in the request.'}, status=status.HTTP_400_BAD_REQUEST)
+
+#         project.save()
+#         queryset = project.files.all()
+#         serializer_class = ProjectFilesSerializer(queryset, many=True)
+#         return JsonResponse(serializer_class.data, safe=False)
+    
+#     except BaseProject.DoesNotExist:
+#         return JsonResponse({'error': 'Project does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+    
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+# @api_view(['POST'])
+# def delete_file(request):
+#     try:
+#         file_id = request.POST['fileId']
+#         project_id = request.POST['projectId']
+        
+#         try:
+#             delete_file = FileUpload.objects.get(pk=file_id)
+#         except FileUpload.DoesNotExist:
+#             return JsonResponse({'error': 'File does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        
+#         delete_file.delete()
+        
+#         try:
+#             files = BaseProject.objects.get(pk=project_id)
+#         except BaseProject.DoesNotExist:
+#             return JsonResponse({'error': 'Project does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+        
+#         queryset = files.files.all()
+#         serializer = ProjectFilesSerializer(queryset, many=True)
+#         return JsonResponse(serializer.data, safe=False)
+    
+#     except Exception as e:
+#         return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class FilesUploadView(APIView):
+    def get(self, request, project_id):
+        try:
+            project_files = BaseProject.objects.get(pk=project_id)
+            queryset = project_files.files.all()
+            serializer_class = ProjectFilesSerializer(queryset, many=True)
+            return JsonResponse(serializer_class.data,safe=False)
+        except BaseProject.DoesNotExist:
+            return JsonResponse({'error': 'Project not found'}, status=status.HTTP_404_NOT_FOUND)
+
+    def post(self, request,project_id):
+        action = request.POST['action']
+
+        if action == 'add_new_file':
+            try:
+                project_id = request.data['projectId']
+                project = BaseProject.objects.get(pk=project_id)
+                
+                if 'files' in request.FILES:
+                    file_list = request.FILES.getlist('files')
+                    file_names = request.POST.getlist('fileNames')
+
+                    for i in range(len(file_list)):
+                        current_file = FileUpload.objects.create(file_name=file_names[i], file=file_list[i])
+                        project.files.add(current_file)
+
+                elif 'file' in request.FILES:
+                    file = request.FILES['file']
+                    file_name = request.POST['fileName']
+                    new_file = FileUpload.objects.create(file_name=file_name, file=file)
+                    project.files.add(new_file)
+
+                else:
+                    return JsonResponse({'error': 'No files were provided in the request.'}, status=status.HTTP_400_BAD_REQUEST)
+
+                project.save()
+                queryset = project.files.all()
+                serializer_class = ProjectFilesSerializer(queryset, many=True)
+                return JsonResponse(serializer_class.data, safe=False)
+                
+            except BaseProject.DoesNotExist:
+                return JsonResponse({'error': 'Project does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+            
+            except Exception as e:
+                return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        elif action == 'delete_file':
+            try:
+                file_id = request.POST['fileId']
+                project_id = request.POST['projectId']
+                
+                try:
+                    delete_file = FileUpload.objects.get(pk=file_id)
+                except FileUpload.DoesNotExist:
+                    return JsonResponse({'error': 'File does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+                
+                delete_file.delete()
+                
+                try:
+                    files = BaseProject.objects.get(pk=project_id)
+                except BaseProject.DoesNotExist:
+                    return JsonResponse({'error': 'Project does not exist.'}, status=status.HTTP_404_NOT_FOUND)
+                
+                queryset = files.files.all()
+                serializer = ProjectFilesSerializer(queryset, many=True)
+                return JsonResponse(serializer.data,safe=False)
+            
+            except Exception as e:
+                return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
         else:
-            return JsonResponse({'error': 'No files were provided in the request.'}, status=status.HTTP_400_BAD_REQUEST)
-
-        project.save()
-        queryset = project.files.all()
-        serializer_class = MyModelSerializer(queryset, many=True)
-        return JsonResponse(serializer_class.data, safe=False)
-    
-    except BaseProject.DoesNotExist:
-        return JsonResponse({'error': 'Project does not exist.'}, status=status.HTTP_404_NOT_FOUND)
-    
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-@api_view(['POST'])
-def delete_file(request):
-    try:
-        file_id = request.POST['fileId']
-        project_id = request.POST['projectId']
-        
-        try:
-            delete_file = FileUpload.objects.get(pk=file_id)
-        except FileUpload.DoesNotExist:
-            return JsonResponse({'error': 'File does not exist.'}, status=status.HTTP_404_NOT_FOUND)
-        
-        delete_file.delete()
-        
-        try:
-            files = BaseProject.objects.get(pk=project_id)
-        except BaseProject.DoesNotExist:
-            return JsonResponse({'error': 'Project does not exist.'}, status=status.HTTP_404_NOT_FOUND)
-        
-        queryset = files.files.all()
-        serializer = MyModelSerializer(queryset, many=True)
-        return JsonResponse(serializer.data, safe=False)
-    
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+            return JsonResponse({'error': 'Invalid action'}, status=status.HTTP_400_BAD_REQUEST)
